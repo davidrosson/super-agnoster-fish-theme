dc4f19d05b6315d0de9b857a2c66f885ce5b7409
David Rosson <david.rosson@gmail.com>
2018-11-11 09:48:58 +0200

    Initial commit

---
+++ fish_right_prompt.fish
@@ -0,0 +1,18 @@
+# Right prompt for agnoster theme
+
+function fish_right_prompt
+  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+
+  if test -n "$AWS_ROLE_NAME" -a -n "$AWS_SESSION_DURATION" -a -n "$AWS_SESSION_START_TIME"
+    set diff (math $AWS_SESSION_DURATION - (math (date +%s) - $AWS_SESSION_START_TIME))
+
+    if math "$diff>0" > /dev/null
+      echo "["$AWS_ROLE_NAME"|"$diff"s] "
+    else
+      aws-clear
+    end
+  end
+
+  date "+%H:%M:%S"
+  set_color normal
+end
